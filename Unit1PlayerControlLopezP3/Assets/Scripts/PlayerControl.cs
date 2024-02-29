24c5a38e9a53a58233c3a0f6d1d102f227c98da7
Armando Lopez <722925@student.dusd.net>
2024-02-29 11:37:42 -0800

    added more obstacles
    added it so I can turn the vehicle and moving obstacles

--- Unit1PlayerControlLopezP3/Assets/Scripts/PlayerControl.cs
+++ Unit1PlayerControlLopezP3/Assets/Scripts/PlayerControl.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +21,18 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the vehicle forward
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Move the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
